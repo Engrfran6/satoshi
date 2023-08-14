e5cba3a3d11a07c06b5af90e449ef043a47c980c
Francis <99579671+Engrfran6@users.noreply.github.com>
2023-08-14 11:21:34 +0100

    fixed cors error and updated the auth-controller.js file

--- back-end/controllers/auth-controller.js
+++ back-end/controllers/auth-controller.js
@@ -1,41 +1,43 @@
 const Joi = require('@hapi/joi');
-const bcrypt = require('bcryptjs')
-const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcryptjs');
 
-const User = require('../models/Users/UserModel')
+const User = require('../models/Users/UserModel');
 const Package = require('../models/Packages/PackageModel');
-
 
 const createUserSchema = Joi.object().keys({
   fullName: Joi.string().required(),
+  password: Joi.string().required(),
   username: Joi.string().required(),
   phoneNumber: Joi.string().required(),
   packageId: Joi.string().required(),
   country: Joi.string().required(),
   state: Joi.string().required(),
-  email: Joi.string().required().email({ tlds: { allow: false } }),
+  email: Joi.string()
+    .required()
+    .email({tlds: {allow: false}}),
 });
 
 const LoginSchema = Joi.object().keys({
   password: Joi.string().required(),
-  email: Joi.string().email({ tlds: { allow: false } }),
+  email: Joi.string().email({tlds: {allow: false}}),
 });
-
 
 exports.register = async (req, res) => {
   try {
     const doc = req.body;
-    const { error } = createUserSchema.validate({...doc});
+    const {error} = createUserSchema.validate({...doc});
     if (error) {
-      return res.status(400).json({ error: error.details[0].message });
+      return res.status(400).json({error: error.details[0].message});
     }
 
-    const package = await Package.findOne({ _id: doc.packageId })
+    const package = await Package.findOne({_id: doc.packageId});
     if (!package) {
-      return res.status(400).json({ error: `no package found with id ${doc.packageId}` });
+      return res.status(400).json({error: `no package found with id ${doc.packageId}`});
     }
-    const generatedPassword = uuidv4()
+
     const salt = await bcrypt.genSalt(10);
+    const generatedPassword = await bcrypt.hash(doc.password, salt);
+    const genrateReferal = generateRandomNumber() + doc.username;
 
     const params = {
       email: doc.email,
@@ -46,22 +48,23 @@ exports.register = async (req, res) => {
       referal: doc.phoneNumber,
       country: doc.country,
       state: doc.state,
-      password: await bcrypt.hash(generatedPassword, salt)
-    }
+      password: generatedPassword,
+      referal: genrateReferal,
+    };
 
     const user = new User(params);
     await user.save();
-    delete user.password
+    delete user.password;
     if (user) {
       return res.status(201).json({
         status: 'success',
-        data: user
+        data: user,
       });
     }
   } catch (e) {
     res.status(500).json({
-      status: "failed",
-      message: e.message
+      status: 'failed',
+      message: e.message,
     });
   }
 };
@@ -69,26 +72,32 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const doc = req.body;
-    const { error } = LoginSchema.validate({...doc});
+    const {error} = LoginSchema.validate({...doc});
     if (error) {
-      return res.status(400).json({ error: error.details[0].message });
+      return res.status(400).json({error: error.details[0].message});
     }
-    const { email, password } = req.body;
-    const user = await User.findOne({ email: email });
+    const {email, password} = req.body;
+    const user = await User.findOne({email: email});
     if (!user) {
-      res.status(401).json({ error: "invalid credentials"});
+      res.status(401).json({error: 'invalid credentials'});
     }
     const isMatch = await user.matchPassword(password);
     if (!isMatch) {
-      res.status(401).json({ error: "invalid credentials"});
+      res.status(401).json({error: 'invalid credentials'});
     } else {
       const token = user.getSignedJwtToken();
       res.status(200).json({
-        status: "success",
-        token
+        status: 'success',
+        token,
       });
     }
-  } catch (e) {
-    
+  } catch (e) {}
+};
+
+const generateRandomNumber = () => {
+  const randomNumbers = [];
+  for (let i = 0; i < 5; i++) {
+    randomNumbers.push(Math.floor(Math.random() * 10));
   }
-}
+  return randomNumbers.join('');
+};
